403a29b8a37d49e38ee87121f28cf80b4506d274
Denis Karev <dkarev@protonmail.com>
2020-11-18 22:30:57 +0100

    string to CommandName, getting CommandArgsType from CommandName, getting CommandType from CommandName

--- sps.c
+++ sps.c
@@ -46,6 +46,7 @@ typedef enum _commandArgsType {
     None,
     String,
     TwoInt,
+    Variable
 } CommandArgsType;
 //endregion
 
@@ -56,6 +57,7 @@ typedef struct _command {
     CommandArgsType command_args_type;
     char string_arg[1000]; // if command_args_type is String
     int int_args[2]; // if command_args_type is TwoInt
+    int variable; // if command_args_type is Variable
 } Command;
 //endregion
 
@@ -90,6 +92,96 @@ void read_delims(int argc, char *argv[])
             return;
         }
     }
+}
+
+CommandName str_to_cmd_name(char *command)
+{
+    if (strcmp(command, "irow") == 0)       return irow;
+    else if (strcmp(command, "arow") == 0)  return arow;
+    else if (strcmp(command, "drow") == 0)  return drow;
+    else if (strcmp(command, "icol") == 0)  return icol;
+    else if (strcmp(command, "acol") == 0)  return acol;
+    else if (strcmp(command, "dcol") == 0)  return dcol;
+    else if (strcmp(command, "set") == 0)   return set;
+    else if (strcmp(command, "clear") == 0) return clear;
+    else if (strcmp(command, "swap") == 0)  return swap;
+    else if (strcmp(command, "sum") == 0)   return sum;
+    else if (strcmp(command, "avg") == 0)   return avg;
+    else if (strcmp(command, "count") == 0) return count;
+    else if (strcmp(command, "len") == 0)   return len;
+    else if (strcmp(command, "def") == 0)   return def;
+    else if (strcmp(command, "use") == 0)   return use;
+    else if (strcmp(command, "inc") == 0)   return inc;
+    else if (strcmp(command, "[set]") == 0) return var_set;
+    else                                    return -1;
+}
+
+CommandArgsType get_args_type(CommandName command)
+{
+    switch (command)
+    {
+    case irow:
+        return None;
+    case arow:
+        return None;
+    case drow:
+        return None;
+    case icol:
+        return None;
+    case acol:
+        return None;
+    case dcol:
+        return None;
+    case set:
+        return String;
+    case clear:
+        return None;
+    case swap:
+        return TwoInt;
+    case sum:
+        return TwoInt;
+    case avg:
+        return TwoInt;
+    case count:
+        return TwoInt;
+    case len:
+        return TwoInt;
+    case def:
+        return Variable;
+    case use:
+        return Variable;
+    case inc:
+        return Variable;
+    case var_set:
+        return None;
+    default:
+        return -1;
+    }
+}
+
+CommandType get_cmd_type(CommandName command)
+{
+    if (command >= irow && command <= dcol)
+    {
+        return ChangeStructure;
+    }
+    else if (command >= set && command <= len)
+    {
+        return ChangeContent;
+    }
+    else if (command >= def && command <= var_set)
+    {
+        return VariableOperation;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+Command str_to_cmd(char *input)
+{
+
 }
 
 int main(int argc, char *argv[])
